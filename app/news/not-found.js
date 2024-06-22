871f27ba58871280da686552fe085b2dd12806a9
조수훈 <soohoon.cho@hanwha.com>
2024-06-23 00:54:34 +0900

    feat: archive 병렬 라우팅 추가

---
+++ app/news/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+      <div id="error">
+        <h1>Not Found!</h1>
+        <p>ㅎㅎ.. resource could not be found!</p>
+      </div>
+    );
+  }
--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,23 +1,11 @@
 import { DUMMY_NEWS } from '@/dummy-news';
-import Link from 'next/link';
+import NewsList from '@/components/news-list';
 
 export default function NewsPage() {
-    return (
-        <>
-            <h1>News Page</h1>
-            <ul className='news-list'>
-                {DUMMY_NEWS.map((newsItem) => (
-                    <li key={newsItem.key}>
-                        <Link href={`/news/${newsItem.slug}`}>
-                            <img
-                                src={`images/news/${newsItem.image}`}
-                                alt={newsItem.title}
-                                />
-                                <span>{newsItem.title}</span>
-                                </Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
+  return (
+    <>
+      <h1>News Page</h1>
+      <NewsList news={DUMMY_NEWS} />
+    </>
+  );
 }
